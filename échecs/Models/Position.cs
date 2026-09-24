8620b1401f6c0b73ad0ed0a32e0ca8a3d37c4feb
KurisuOnFire <75785399+Serral-sniper@users.noreply.github.com>
2026-09-24 20:18:50 +0200

    Board + update Position.cs

--- échecs/Models/Position.cs
+++ échecs/Models/Position.cs
@@ -2,14 +2,14 @@ namespace échecs.Models;
 
 public class Position
 {
-    public int _X { get; private set; }
-    public int _Y { get; private set; }
+    public int X { get; private set; }
+    public int Y { get; private set; }
 
     
     public Position(int x, int y)
     {
-        _X = x;
-        _Y = y;
+        X = x;
+        Y = y;
     }
     
 }
